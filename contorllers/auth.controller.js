c6f727fc0d2e694bd3e40130afaeeb6c71123e31
gaurav1Nn <gauravnilawar26@gmail.com>
2025-05-18 11:27:47 +0530

    adding all the files

---
+++ contorllers/auth.controller.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const registerUser = async (req, res) => {
+    console.log("User registered");
+    await prisma.user.findUnique({
+        where : {
+            email 
+        }
+    })
+};
+
